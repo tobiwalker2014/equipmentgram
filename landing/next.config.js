63c4b943780a550d2cdb1086c9cdd07b0f5bdfdc
shibli21 <syedshiblimahmud@gmail.com>
2023-10-30 01:12:05 +0600

    new next app

--- landing/next.config.js
+++ landing/next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+const nextConfig = {}
 
-    return config;
-  },
-};
+module.exports = nextConfig
