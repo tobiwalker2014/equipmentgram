f20bbb4ed400e9bcd734fdf335bfd41c78ac065b
syed shibli mahmud <syedshiblimahmud@gmail.com>
2023-11-21 23:31:10 +0600

    loading screen

---
+++ landing/app/admin/loading.tsx
@@ -0,0 +1,10 @@
+import CustomLoader from "@/components/Loader";
+import React from "react";
+
+type Props = {};
+
+const Loading = (props: Props) => {
+  return <CustomLoader />;
+};
+
+export default Loading;
